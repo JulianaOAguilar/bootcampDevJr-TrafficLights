590931deb8eaf0389b78cf70ad85a88161486b3e
Xinlana1 <julianaoaguilar16@gmail.com>
2024-09-11 10:24:29 -0300

    Close #3. JavaScript

---
+++ javascript.js
@@ -0,0 +1,25 @@
+
+
+
+
+function changetrafficlight(){
+    if(document.getElementById("yellow").className === "light yellow-light") {
+        document.getElementById("red").className = "light red-light";
+        document.getElementById("yellow").className = "light light-off";
+    }
+
+ else {
+    if(document.getElementById("red").className === "light red-light") {
+        document.getElementById("red").className = "light light-off";
+        document.getElementById("green").className = "light green-light";
+    }
+    
+    
+    else {
+        document.getElementById("green").className = "light light-off";
+        document.getElementById("yellow").className = "light yellow-light";
+        }
+    }
+ }
+
+
